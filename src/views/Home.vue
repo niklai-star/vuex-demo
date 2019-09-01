78f152834c8ddb2b1ae895db618ffef4efef8757
niklai.li <niklai@163.com>
2019-09-01 10:44:11 +0800

    初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <div>{{ countToString }}</div>
+    <div>{{ count }}</div>
+  </div>
+</template>
+
+<script>
+import { createNamespacedHelpers } from "vuex";
+const { mapGetters, mapState } = createNamespacedHelpers("home");
+
+export default {
+  name: "Home",
+  computed: {
+    ...mapState(["count"]),
+    ...mapGetters(["countToString"])
+  },
+  components: {},
+  methods: {}
+};
+</script>
